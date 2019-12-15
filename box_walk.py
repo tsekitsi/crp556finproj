68cb6a1876e9a5c16d79b099db932317fa650440
tsekitsi <tseki@iastate.edu>
2019-12-15 16:21:17 -0600

    Data downloading done

---
+++ box_walk.py
@@ -0,0 +1,55 @@
+# Author:   Marios Tsekitsidis
+# Date:     December 16, 2019
+# Version:  Python 2.7.13
+# Purpose:  This script was written as part of the Final Project in C R P 556 (F19)
+
+
+import requests
+
+
+class BoxWalker:
+    # parameters for request to Box API:
+    dev_token = None  # requires box dev application (expires every hr)
+    shared_folder_url = None  # shared folder to walk
+    headers = None  # headers to be used in GET request
+    discovered = set()  # for Depth First Search
+    files = {}  # for Depth First Search
+
+    def __init__(self, shared_folder_url, dev_token):
+        self.dev_token = dev_token
+        self.shared_folder_url = shared_folder_url
+        self.headers = {'Authorization': 'Bearer ' + self.dev_token,
+                        'BoxApi': 'shared_link=' + self.shared_folder_url}
+
+    # function 'get_items' returns two lists; one with IDs of sub-folders and one with IDs of files in folder:
+    def get_items(self, folder_id):
+        folders = []
+        files = []
+        items = requests.get('https://api.box.com/2.0/folders/' + folder_id + '/items', headers=self.headers)
+        as_dict = items.json()
+        # for each item (file/folder) in the folder:
+        for item in as_dict['entries']:
+            if item['type'] == 'folder':
+                folders.append(item['id'])
+            if item['type'] == 'file':
+                files.append((item['id'], item['name']))
+        return folders, files
+
+    def dfs(self, folder_id, filters):
+        print 'now in folder ' + folder_id  # TO-DO: convert to logger
+        self.discovered.add(folder_id)
+        new_folders, new_files = self.get_items(folder_id)
+        for nf in new_files:
+            if filters:
+                if filters['contains'] in nf[1] and nf[1].endswith(filters['endswith']):
+                    self.files[nf[0]] = nf[1]
+            else:
+                self.files[nf[0]] = nf[1]
+        for sub_dir in new_folders:
+            if sub_dir not in self.discovered:
+                self.dfs(sub_dir, filters)
+
+    def walk(self, folder_id, filters=None):
+        print 'Walking folder ' + folder_id + '...'
+        self.dfs(folder_id, filters)
+        return [(id_, name) for id_, name in self.files.items()]
---
+++ marios_finproj.py
@@ -0,0 +1,66 @@
+# Author:   Marios Tsekitsidis
+# Date:     December 16, 2019
+# Version:  Python 2.7.13
+# Purpose:  This script was written as part of the Final Project in C R P 556 (F19)
+
+
+import arcpy
+import datetime
+import os
+import subprocess
+import time
+from tqdm import tqdm
+import zipfile  # import standard modules
+from box_walk import *  # import custom module
+
+
+# 0. Setup
+
+
+arcpy.CheckOutExtension('Spatial')  # check out Spatial Analyst extension
+
+
+# 1. Download data
+
+
+start = time.time()
+os.chdir('data')  # step into data dir
+
+# (a) download Iowa DEM data:
+
+bw = BoxWalker('https://iastate.box.com/s/dboob8jvve6qvk639smhbpsw0b7g4qbf', '5ZAvdQOpUaVeZBLlz2MYIY8BeKLNfiWW')
+for x in tqdm(bw.walk('52245723277', filters={'contains': 'DEM', 'endswith': '.zip'}), 'Downloading counties DEM...'):
+    file_id, file_name = x
+    try:
+        r = requests.get('https://api.box.com/2.0/files/' + file_id + '/content', headers=bw.headers)
+        with open(os.path.join('DEM_raw', file_name), 'wb') as y:
+            y.write(r.content)
+        with zipfile.ZipFile(os.path.join('DEM_raw', file_name), 'r') as z:
+            z.extractall('DEM')
+    except requests.exceptions.RequestException as e:
+        print e  # print encountered error
+
+# (b) download hydrology data for the VPUs which Iowa is a part of:
+
+base_url = 'http://www.horizon-systems.com/NHDPlusData/NHDPlusV21/Data/NHDPlusMS/'
+rel_urls = ['NHDPlus07/NHDPlusV21_MS_07_VogelExtension_01.7z',  # Upper Mississippi 07
+            'NHDPlus10U/NHDPlusV21_MS_10U_VogelExtension_02.7z',  # Upper Missouri 10U
+            'NHDPlus10L/NHDPlusV21_MS_10L_VogelExtension_01.7z']  # Lower Missouri 10L
+
+for vpu in rel_urls:
+    try:
+        r = requests.get(base_url + vpu)
+        archive_name = vpu.split('/')[1]
+        with open(archive_name, 'wb') as f:
+            f.write(r.content)
+        subprocess.call(
+            r'"..\helpers\7-ZipPortable\App\7-Zip64\7z.exe" x ' + archive_name + ' -aoa' + ' -o' + 'NHDPlusMS_raw',
+            stdout=open(os.devnull, 'wb')  # silent mode
+        )
+    except requests.exceptions.RequestException as e:
+        print e  # print encountered error # TO-DO: change to logger
+
+os.chdir('..')  # return to root dir
+
+elapsed = time.time() - start
+print 'Time elapsed downloading data: '+str(datetime.timedelta(seconds=elapsed))
