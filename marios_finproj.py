74f7e26d80e36952c8d5103cdebc2c184ce6b720
tsekitsi <tseki@iastate.edu>
2019-12-15 21:16:36 -0600

    All good downloading and early pre-processing

--- marios_finproj.py
+++ marios_finproj.py
@@ -11,11 +11,26 @@ import time
 import zipfile  # import standard modules
 from box_walk import *  # import custom module
 
-# 0. Setup
+############
+# 0. Setup #
+############
+
+# check if output geodatabase exists; if not, create it:
+cwd = os.getcwd()
+hydroGDB = os.path.join(cwd, 'iowa_hydro.gdb')
+if not arcpy.Exists(hydroGDB):
+    arcpy.CreateFileGDB_management(os.path.dirname(hydroGDB), os.path.basename(hydroGDB))
+
+# set up environments:
+arcpy.env.scratchWorkspace = 'in_memory'
+arcpy.env.workspace = hydroGDB
+arcpy.env.overwriteOutput = True
 
 arcpy.CheckOutExtension('Spatial')  # check out Spatial Analyst extension
 
-# 1. Download data
+#######################
+# 1. Downloading data #
+#######################
 
 start = time.time()
 
@@ -29,29 +44,29 @@ for d in subdirs:
     if not os.path.isdir(d):
         os.mkdir(d)
 
-# (a) download Iowa DEM data:
-
-print 'Downloading DEM data...'
-
-bw = BoxWalker('https://iastate.box.com/s/dboob8jvve6qvk639smhbpsw0b7g4qbf', '')
-
-files = bw.walk('52245723277', filters={'contains': 'DEM', 'endswith': '.zip'})
-for i in range(len(files)):
-    if i % 10 == 0:
-        print '  Downloading DEM for county ' + str(i + 1) + '/' + str(len(files)) + ' (' + str(
-            len(files) - i) + ' left)...'
-    file_id, file_name = files[i]
-    try:
-        r = requests.get('https://api.box.com/2.0/files/' + file_id + '/content', headers=bw.headers, stream=True)
-        with open(os.path.join('DEM_raw', file_name), 'wb') as f:
-            for block in r.iter_content(1024):
-                f.write(block)
-        with zipfile.ZipFile(os.path.join('DEM_raw', file_name), 'r') as z:
-            z.extractall('DEM')
-    except requests.exceptions.RequestException as e:
-        print e  # print encountered error
-
-print 'Done!'
+# # (a) download Iowa DEM data:
+#
+# print 'Downloading DEM data...'
+#
+bw = BoxWalker('https://iastate.box.com/s/dboob8jvve6qvk639smhbpsw0b7g4qbf', 'sPt87tzT5k8VXnUyNcpoLqDuEf2Ftm3p')
+#
+# files = bw.walk('52245723277', filters={'contains': 'DEM', 'endswith': '.zip'})
+# for i in range(len(files)):
+#     if i % 10 == 0:
+#         print '  Downloading DEM for county ' + str(i + 1) + '/' + str(len(files)) + ' (' + str(
+#             len(files) - i) + ' left)...'
+#     file_id, file_name = files[i]
+#     try:
+#         r = requests.get('https://api.box.com/2.0/files/' + file_id + '/content', headers=bw.headers, stream=True)
+#         with open(os.path.join('DEM_raw', file_name), 'wb') as f:
+#             for block in r.iter_content(1024):
+#                 f.write(block)
+#         with zipfile.ZipFile(os.path.join('DEM_raw', file_name), 'r') as z:
+#             z.extractall('DEM')
+#     except requests.exceptions.RequestException as e:
+#         print e  # print encountered error
+#
+# print 'Done!'
 
 # (b) download shapefile of Iowa flowline:
 
@@ -60,7 +75,8 @@ print 'Downloading hydrology data...'
 try:
     r = requests.get('https://api.box.com/2.0/files/310185097791/content', headers=bw.headers, stream=True)
     with open('NHD_flowline_raw.zip', 'wb') as f:
-        f.write(r.content)
+        for block in r.iter_content(1024):
+            f.write(block)
     with zipfile.ZipFile('NHD_flowline_raw.zip', 'r') as z:
         z.extractall()
 except requests.exceptions.RequestException as e:
@@ -77,12 +93,15 @@ for vpu in rel_urls:
     try:
         r = requests.get(base_url + vpu, stream=True)
         archive_name = vpu.split('/')[1]
-        with open(os.path.join('NHDPlusMS_raw', archive_name), 'wb') as f:
-            f.write(r.content)
+        os.chdir('NHDPlusMS_raw')
+        with open(archive_name, 'wb') as f:
+            for block in r.iter_content(1024):
+                f.write(block)
         subprocess.call(
-            r'"..\helpers\7-ZipPortable\App\7-Zip64\7z.exe" x ' + archive_name + ' -aoa' + ' -o' + 'NHDPlusMS',
+            r'"..\..\helpers\7-ZipPortable\App\7-Zip64\7z.exe" x ' + archive_name + ' -aoa' + ' -o' + '..',
             stdout=open(os.devnull, 'wb')  # silent mode
         )
+        os.chdir('..')
     except requests.exceptions.RequestException as e:
         print e  # print encountered error # TO-DO: change to logger
 
@@ -91,4 +110,23 @@ os.chdir('..')  # return to root dir
 print('Done!')
 
 elapsed = time.time() - start
-print 'Time elapsed downloading data: ' + str(datetime.timedelta(seconds=elapsed))
+print 'Time elapsed downloading and extracting data: ' + str(datetime.timedelta(seconds=elapsed))
+
+#####################
+# 2. Pre-processing #
+#####################
+
+# copy tables into GDB:
+for root, dirs, files in os.walk(os.path.join('data', 'NHDPlusMS')):
+    for f in files:
+        if f.endswith('.dbf'):
+            in_dbf = os.path.join(cwd, root, f)
+            out_dbf = os.path.basename(os.path.dirname(root))
+            out_dbf = os.path.join(cwd, hydroGDB, out_dbf)
+            arcpy.CopyRows_management(in_dbf, out_dbf)
+
+# copy shp into GDB:
+in_shp = os.path.join(cwd, 'data', 'NHD_flowline.shp')
+out_shp = os.path.join(cwd, hydroGDB, 'NHD_flowline')
+arcpy.CopyFeatures_management(in_shp, out_shp)
+
